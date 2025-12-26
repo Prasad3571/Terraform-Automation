90c7fb510a0976a7d4c165c2079605d93ff074a2
Prasad3571 <shindeprasad3567@gmail.com>
2025-12-26 17:25:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "aws_instance" "myFirstInstance" {
   ami           = var.ami_id
   key_name = var.key_name
   instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.jenkins-sg-2021.id]
+  vpc_security_group_ids = [aws_security_group.jenkins-sgroup-dec-2021.id]
 
   # Set root volume size to 20 GB
   root_block_device {
